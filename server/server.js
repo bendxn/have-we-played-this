919b269098223f1baef05ded37d93e6028b66f6a
Ben Dixon <bendxn@gmail.com>
2015-02-13 22:54:32 +0000

    wiring up databasae to search correctly

--- server/server.js
+++ server/server.js
@@ -1,13 +1,6 @@
-Songs = new Mongo.Collection('songs');
-Songs.initEasySearch(['title', 'artist'], {
-	'limit' : 20,
-	'use' : 'mongo-db'
+Meteor.startup(function () {
+	if (Songs.find().count() === 0) {
+		Songs.insert({ title: 'I Believe in a Thing Called Love', artist: 'The Darkness', lastYearPlayed: 2015 });
+	}
 });
 
-if (Meteor.isServer) {
-	Meteor.startup(function () {
-		if (Songs.find().count() === 0) {
-			Songs.insert({ title: 'I Believe in a Thing Called Love', artist: 'The Darkness', lastYearPlayed: 2015 });
-		}
-	});
-}
