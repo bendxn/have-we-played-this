8e56d8d2aaa830ce754782dc24f98900f296199b
Ben Dixon <bendxn@gmail.com>
2015-02-14 00:53:33 +0000

    styling for page

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
 	if (Songs.find().count() === 0) {
-		Songs.insert({ title: 'I Believe in a Thing Called Love', artist: 'The Darkness', lastYearPlayed: 2015 });
+		Songs.insert({ title: 'Africa', artist: 'Toto', lastYearPlayed: 2014 });
 	}
 });
 
